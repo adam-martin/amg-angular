96e78c32c4d1f8d2afbdca2e0fcca51190f4dc81
amartin <im.adam.martin@gmail.com>
2015-02-16 19:06:34 +0700

    main module

--- templates/scripts/app/app.js
+++ templates/scripts/app/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/**
+ * @ngdoc overview
+ * @name <%= appName %>
+ * @description <%= appDesription %>
+ * # <%= appName %>
+ *
+ * Main module of the application.
+ */
+
 angular
     .module('<%= appName %>', [
         'ngRoute'
